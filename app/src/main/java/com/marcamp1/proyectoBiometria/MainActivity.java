56ffe9592eb24eca6b8951c8ef285e152d89337c
Marc <mcampins28@gmail.com>
2021-10-07 17:30:11 +0200

    Initial commit

---
+++ app/src/main/java/com/marcamp1/proyectoBiometria/MainActivity.java
@@ -0,0 +1,14 @@
+package com.marcamp1.proyectoBiometria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
